54fed24958c251e8e8b347505b0d67192319701b
Shubham Kumar <skumar@monarchtractor.com>
2024-04-11 13:16:27 +0530

    updated incremental_mapping signature

--- pipeline/sfm.cc
+++ pipeline/sfm.cc
@@ -46,6 +46,68 @@ Reconstruction triangulate_points(Reconstruction reconstruction,
     return reconstruction;
 }
 
+
+// Copied from colmap/exe/sfm.cc
+std::map<size_t, Reconstruction> incremental_mapping(
+    const py::object database_path_,
+    const py::object image_path_,
+    const py::object output_path_,
+    const py::object gps_priors_path_, 
+    const IncrementalMapperOptions& options,
+    const py::object input_path_) {
+    
+    std::string database_path = py::str(database_path_).cast<std::string>();
+    THROW_CHECK_FILE_EXISTS(database_path);
+    std::string image_path = py::str(image_path_).cast<std::string>();
+    THROW_CHECK_DIR_EXISTS(image_path);
+    std::string input_path = py::str(input_path_).cast<std::string>();  
+    std::string output_path = py::str(output_path_).cast<std::string>();
+    std::string gps_priors_path = py::str(gps_priors_path_).cast<std::string>();    
+    CreateDirIfNotExists(output_path);
+
+    py::gil_scoped_release release;
+    ReconstructionManager reconstruction_manager;
+    if (input_path != "") {
+        THROW_CHECK_DIR_EXISTS(input_path);
+        reconstruction_manager.Read(input_path);
+    }
+    IncrementalMapperController mapper(
+        &options, image_path, database_path, gps_priors_path, &reconstruction_manager);
+
+    // In case a new reconstruction is started, write results of individual sub-
+    // models to as their reconstruction finishes instead of writing all results
+    // after all reconstructions finished.
+    size_t prev_num_reconstructions = 0;
+    std::map<size_t, Reconstruction> reconstructions;
+    mapper.AddCallback(
+        IncrementalMapperController::LAST_IMAGE_REG_CALLBACK, [&]() {
+            // If the number of reconstructions has not changed, the last model
+            // was discarded for some reason.
+            if (reconstruction_manager.Size() > prev_num_reconstructions) {
+                const std::string reconstruction_path = JoinPaths(
+                    output_path, std::to_string(prev_num_reconstructions));
+                const auto& reconstruction =
+                    reconstruction_manager.Get(prev_num_reconstructions);
+                CreateDirIfNotExists(reconstruction_path);
+                reconstruction.Write(reconstruction_path);
+                reconstructions[prev_num_reconstructions] = reconstruction;
+                prev_num_reconstructions = reconstruction_manager.Size();
+            }
+        });
+
+    PyInterrupt py_interrupt(1.0);  // Check for interrupts every 2 seconds
+    mapper.AddCallback(IncrementalMapperController::NEXT_IMAGE_REG_CALLBACK,
+                       [&]() {
+                           if (py_interrupt.Raised()) {
+                               throw py::error_already_set();
+                           }
+                       });
+
+    mapper.Start();
+    mapper.Wait();
+    return reconstructions;
+}
+
 // Copied from colmap/exe/sfm.cc
 std::map<size_t, Reconstruction> incremental_mapping(
     const py::object database_path_,
@@ -57,7 +119,7 @@ std::map<size_t, Reconstruction> incremental_mapping(
     THROW_CHECK_FILE_EXISTS(database_path);
     std::string image_path = py::str(image_path_).cast<std::string>();
     THROW_CHECK_DIR_EXISTS(image_path);
-    std::string input_path = py::str(input_path_).cast<std::string>();
+    std::string input_path = py::str(input_path_).cast<std::string>();  
     std::string output_path = py::str(output_path_).cast<std::string>();
     CreateDirIfNotExists(output_path);
 
@@ -193,6 +255,23 @@ void init_sfm(py::module& m) {
           py::arg("options") = mapper_options,
           "Triangulate 3D points from known camera poses");
 
+    m.def("incremental_mapping",
+          static_cast<std::map<size_t, Reconstruction> (*)(
+              const py::object,
+              const py::object,
+              const py::object,
+              const py::object,
+              const IncrementalMapperOptions&,
+              const py::object)>(&incremental_mapping),
+          py::arg("database_path"),
+          py::arg("image_path"),
+          py::arg("output_path"),
+          py::arg("gps_priors_path"),
+          py::arg("options") = mapper_options,
+          py::arg("input_path") = py::str(""),
+          "Triangulate 3D points from known poses");
+
+    
     m.def("incremental_mapping",
           static_cast<std::map<size_t, Reconstruction> (*)(
               const py::object,
